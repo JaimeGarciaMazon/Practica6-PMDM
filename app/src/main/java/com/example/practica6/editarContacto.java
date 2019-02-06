ab52ee1181c90d027da2095510b22136e16bc213
jaime <jaimegarcia_24@hotmail.com>
2019-02-06 17:59:45 +0100

    actualizacion

--- app/src/main/java/com/example/practica6/editarContacto.java
+++ app/src/main/java/com/example/practica6/editarContacto.java
@@ -1,7 +1,9 @@
 package com.example.practica6;
 
+import android.annotation.SuppressLint;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
 
 public class editarContacto extends AppCompatActivity {
 
@@ -9,5 +11,12 @@ public class editarContacto extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_editar_contacto);
+    }
+
+    @SuppressLint("RestrictedApi")
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menubloc, menu);
+        return true;
     }
 }
